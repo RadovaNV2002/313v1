edf3aafcd466273d691b47db66c29ba7ab9823f8
RadovaNV2002 <hope.1948@mail.ru>
2025-01-12 16:46:24 +0300

    userPage

--- src/main/java/ru/alexeykedr/springbootbootstrap/repository/UserRepository.java
+++ src/main/java/ru/alexeykedr/springbootbootstrap/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package ru.alexeykedr.springbootbootstrap.dao;
+package ru.alexeykedr.springbootbootstrap.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
